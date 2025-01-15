e266c7b2bce3f9189d8d899b6e996c93caefacdb
Fedekkc <cacacefederico05@gmail.com>
2025-01-15 15:08:07 -0300

    added swagger reference for url/api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Torneo Tenis API')
+    .setDescription('API para la gestión de un torneo de tenis')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
